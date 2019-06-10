5577ebbec95099062d12892af60447d3539479a1
JimBeam2019 <bingking68@hotmail.com>
2019-06-10 12:02:04 +0800

    Completed the basic setup.

--- graphql.js
+++ graphql.js
@@ -1,22 +1,45 @@
-import { ApolloServer, gql } from 'apollo-server';
+import { ApolloServer } from 'apollo-server';
+import { ApolloGateway } from '@apollo/gateway';
 
-const typeDefs = gql`
-    type Query {
-        info: String!
-    }
-`;
-
-const resolvers = {
-  Query: {
-    info: () => `Hello World!`,
-  },
-};
-
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+const gateway = new ApolloGateway({
+  serviceList: [
+    { name: "chef", url:"http://localhost:4001/graphql" },
+    { name: "recipe", url:"http://localhost:4002/graphql" }
+  ]
 });
 
-server.listen().then(({ url }) => {
-  console.log(`Server is ready at ${url}`);
-});
+(async () =>{
+  const {
+    schema, 
+    executor
+  } = await gateway.load();
+  const server = new ApolloServer({
+    schema,
+    executor
+  });
+
+  server.listen().then(({ url }) => {
+    console.log(`Server ready at ${url}`);
+  })
+})();
+
+// const typeDefs = gql`
+//     type Query {
+//         info: String!
+//     }
+// `;
+
+// const resolvers = {
+//   Query: {
+//     info: () => `Hello World!`,
+//   },
+// };
+
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
+
+// server.listen().then(({ url }) => {
+//   console.log(`Server is ready at ${url}`);
+// });
